3e90763336fee1dd7861d60e216b7a9f2c0d6b51
Matheus Sampaio Soier <132232562+Matheus-soier@users.noreply.github.com>
2023-08-03 13:10:00 -0300

    updates

---
+++ JavaScript/JS Básico/main.js
@@ -0,0 +1,7 @@
+function gravidade() {
+  console.log('A gravidade do platena é:')
+  console.log(9.8)
+}
+
+console.log('Opa, tudo bem?')
+gravidade()
---
+++ JavaScript/Primeiros passos/Condinal Ternário/main.js
@@ -0,0 +1,5 @@
+let isMember = true
+let shipping = isMember ? 2 : 10
+
+console.log(isMember ? 'Você é membro' : 'Você não é membro')
+console.log("Frete: " + shipping)
