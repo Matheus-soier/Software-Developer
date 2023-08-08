41d5e57a5cd0f704217a3a2cd9ae36665b055cab
Matheus Sampaio Soier <132232562+Matheus-soier@users.noreply.github.com>
2023-08-08 13:52:40 -0300

    updates

---
+++ JavaScript/JS Básico/Função dentro de objeto/main.js
@@ -0,0 +1,10 @@
+let pessoas = {
+  nome: 'Matheus',
+  sobrenome: 'Soier',
+  idade: 20,
+  nomeCompleto: function () {
+    return `${this.nome} ${this.sobrenome}`
+  }
+}
+
+console.log(pessoas.nomeCompleto())
