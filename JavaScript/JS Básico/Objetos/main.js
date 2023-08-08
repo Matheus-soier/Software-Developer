1cfbb337f378cd44b5d228757b248bf0b57ef70a
Matheus Sampaio Soier <132232562+Matheus-soier@users.noreply.github.com>
2023-08-08 12:55:33 -0300

    update

--- JavaScript/JS Básico/Objetos/main.js
+++ JavaScript/JS Básico/Objetos/main.js
@@ -10,6 +10,13 @@ let personagem = {
   }
 }
 
+// adicionar item no objeto
+personagem.caracteristicas.forca += 5
+
+// adicionar item no array
+
+personagem.olhos.push('verde')
+
 if (personagem.caracteristicas.imortalidade) {
   console.log("Imorta Ativado")
 } else {
@@ -19,3 +26,16 @@ if (personagem.caracteristicas.imortalidade) {
 console.log(`${personagem.nome} tem ${personagem.idade} anos de idade`)
 
 console.log(`Cor dos olhos: ${personagem.olhos[1]}`)
+
+// Parte 2
+
+let personagem02 = {
+  nome: "soier",
+  idade: 20,
+  carros: [
+    { modelo: 'fiati', cor: 'preto' },
+    { modelo: 'ferrari', cor: 'vermelho' }
+  ],
+}
+
+console.log(personagem02.carros[0].cor)
