4b172ea7fddebac8582cddd9b45e3ea7e7098925
Matheus Sampaio Soier <132232562+Matheus-soier@users.noreply.github.com>
2023-08-09 00:00:03 -0300

    updates

---
+++ JavaScript/JS Básico/Funções de array/main.js
@@ -0,0 +1,9 @@
+let fruits = ['Maça', 'Uva', 'Banana', 'Laranja']
+
+//fruits.pop()
+fruits.push('Kiwi')
+fruits.shift()
+
+console.log(fruits.length)
+console.log(fruits.join(', '))
+console.log(fruits)
