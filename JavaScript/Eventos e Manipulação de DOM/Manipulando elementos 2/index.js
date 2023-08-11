a562ccd3f9365fb57fabc91e89658598d0f59126
Matheus Sampaio Soier <132232562+Matheus-soier@users.noreply.github.com>
2023-08-11 13:46:37 -0300

    updates

--- JavaScript/Eventos e Manipulação de DOM/Manipulando elementos 2/index.js
+++ JavaScript/Eventos e Manipulação de DOM/Manipulando elementos 2/index.js
@@ -10,4 +10,25 @@ function clicou() {
 
   //prepend -> adiciona no inicio
   // ul.prepend(newLi)
+
+  const teste2 = document.querySelector('.teste2')
+  const ul2 = teste2.querySelector('ul')
+
+  const newButton = document.createElement('button')
+  newButton.innerText = "Novo botão"
+
+  ul2.before(newButton)
+
+  //BEFORE = ANTES
+  //AFTER = DEPOIS
+
+  let newUl = document.createElement('ul')
+
+  for (let i = 0; i < 5; i++) {
+    let newLista = document.createElement('li')
+    newLista.innerText = "Item add " + (i + 1)
+    newUl.append(newLista)
+  }
+
+  ul.after(newUl)
 }
