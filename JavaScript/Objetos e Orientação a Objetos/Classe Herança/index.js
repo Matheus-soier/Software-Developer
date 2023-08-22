e93876e13d168712a0bdbe3520474c698c072107
Matheus Sampaio Soier <132232562+Matheus-soier@users.noreply.github.com>
2023-08-21 23:31:32 -0300

    updates

---
+++ JavaScript/Objetos e Orientação a Objetos/Classe Herança/index.js
@@ -0,0 +1,32 @@
+class person {
+
+  age = 0;
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHi() {
+    console.log(`${this.name} diz Oi`);
+  }
+}
+
+class student extends person {
+
+  constructor(name, id) {
+    super(name);
+    this.id = id;
+  }
+
+  sayHello() {
+    super.sayHi();
+  }
+}
+
+let p1 = new student("Matheus", 100110110);
+p1.age = 33;
+
+
+p1.sayHello();
+
+console.log(`${p1.name} tem ${p1.age} anos e sua matricula é #${p1.id}.`);
