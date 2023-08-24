a7a014b617c960b134b941533b6aa359b9fa836a
Matheus Sampaio Soier <132232562+Matheus-soier@users.noreply.github.com>
2023-08-24 15:57:33 -0300

    updates

--- JavaScript/Objetos e Orientação a Objetos/Projeto Luta/assets/js/classes.js
+++ JavaScript/Objetos e Orientação a Objetos/Projeto Luta/assets/js/classes.js
@@ -40,3 +40,75 @@ class Sorcerer extends Character {
     this.maxLife = this.life;
   }
 }
+
+class LittleMonster extends Character {
+  constructor() {
+    super(`Little Monster`);
+    this.life = 40;
+    this.attack = 4;
+    this.defense = 4;
+    this.maxLife = this.life;
+  }
+}
+
+class BigMonster extends Character {
+  constructor() {
+    super(`Big Monster`);
+    this.life = 120;
+    this.attack = 16;
+    this.defense = 6;
+    this.maxLife = this.life;
+  }
+}
+
+class Stage {
+  constructor(fighter1, fighter2, fighter1El, fighter2El) {
+    this.fighter1 = fighter1;
+    this.fighter2 = fighter2;
+    this.fighter1El = fighter1El;
+    this.fighter2El = fighter2El;
+  }
+
+  start() {
+    this.update();
+
+    this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+    this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
+  }
+
+  update() {
+    //fighter 1
+    this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`;
+    let f1pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+    this.fighter1El.querySelector('.bar').style.width = `${f1pct}%`
+
+    //fighter 2
+    this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
+    let f2pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+    this.fighter2El.querySelector('.bar').style.width = `${f2pct}%`
+  }
+
+  doAttack(attacking, attacked) {
+    if (attacking.life <= 0 || attacked.life <= 0) {
+      console.log(`Atacando cachorro morto`);
+      return;
+    }
+
+    let attackFactor = (Math.random() * 2).toFixed(2);
+    let defenseFactor = (Math.random() * 2).toFixed(2);
+
+
+    let actualAttack = attacking.attack * attackFactor;
+    let actualDefense = attacked.defense * defenseFactor;
+
+    if (actualAttack > actualDefense) {
+      attacked.life -= actualAttack;
+      console.log(`${attacking.name} causou ${actualAttack} de dano em ${attacked.name}`)
+    } else {
+      console.log(`${attacked.name} conseguiu defender...`)
+    }
+
+    this.update();
+  }
+
+}
--- JavaScript/Objetos e Orientação a Objetos/Projeto Luta/assets/js/index.js
+++ JavaScript/Objetos e Orientação a Objetos/Projeto Luta/assets/js/index.js
@@ -1,6 +1,10 @@
-let char = new Knight('Matheus');
+let char = new Knight('Guerreiro');
+let monster = new BigMonster();
+const stage = new Stage(
+  char,
+  monster,
+  document.querySelector('#char'),
+  document.querySelector('#monster')
+);
 
-console.log(char.name);
-console.log(char.maxLife);
-console.log(char.defense);
-console.log(char.attack);
+stage.start();
