304952cf4a55ab7f893e85fd668ab1405a0458ac
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2023-09-17 23:17:33 -0300

    updates

--- JavaScript/Requisições/Requisição na Pratica/main.js
+++ JavaScript/Requisições/Requisição na Pratica/main.js
@@ -1,11 +1,8 @@
-let button = document.querySelector('#button');
-button.addEventListener('click', clicou);
-
-function clicou() {
+document.querySelector('#button').addEventListener('click', ()=>{
     fetch('https://jsonplaceholder.typicode.com/posts').then((response)=>{
         return response.json();
     })
     .then((json)=>{
         alert(`Titulo do primeiro post: ${json[0].title}`);
     })
-}
+})
