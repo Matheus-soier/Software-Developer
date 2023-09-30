454a03c19e9581b4117c04ba64eb527b54a1cc76
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2023-09-30 16:45:04 -0300

    updates

---
+++ JavaScript/Requisições/Promise/script.js
@@ -0,0 +1,8 @@
+function clicou(){
+    fetch('https://jsonplaceholder.typicode.com/posts').then((response) => {
+        return response.json();
+    }).then((json) => {
+        alert(`Titulo do primeiro post: ${json[0].title}`);
+    })
+    alert(`Opa, clicou!`) //assíncrono
+}
