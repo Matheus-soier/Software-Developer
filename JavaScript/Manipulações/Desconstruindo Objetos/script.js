7527db4d328bcad069ae6804c690ee5d09ffafb0
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2023-11-12 18:39:26 -0300

    Desconstruindo Array

--- JavaScript/Manipulações/Desconstruindo Objetos/script.js
+++ JavaScript/Manipulações/Desconstruindo Objetos/script.js
@@ -10,8 +10,35 @@ let pessoa = {
     },
     nomeCompleto: ()=>{
         return `${pessoa.nome} ${pessoa.sobrenome}`;
+    },
+    endereco: {
+        rua: {
+            nome: "Benjamin Capusso",
+            numero: 247
+        }
     }
 };
 
-let {nome, idade, sobrenome:sobrenomePessoa, social, nomeCompleto, matricula = 23465412379031} = pessoa;
-console.log(nome, sobrenomePessoa, idade, social.tiktok, nomeCompleto(), matricula);
+let {nome, idade, sobrenome:sobrenomePessoa, nomeCompleto, matricula = 23465412379031} = pessoa;
+let {tiktok:ticoteco} = pessoa.social; //Forma 1
+let {social:{facebook:face}} = pessoa; // Forma 2
+let {social} = pessoa; // Forma 3
+
+let {endereco:{rua:{nome:endereco, numero}}} = pessoa;
+console.log(endereco, numero);
+
+console.log(`Forma 1: ${ticoteco}`);
+console.log(`Forma 2: ${face}`)
+console.log(`Forma 3: ${social.facebook} ${social.tiktok}`);
+console.log(`Forma 4: ${pessoa.social.facebook}`); // Forma 4
+
+console.log(nome, sobrenomePessoa, idade, nomeCompleto(), matricula);
+
+//Função Descontruindo Objeto pelo prorpio parâmetro
+function enderecoCompleto({endereco:{rua:{nome:endereco, numero}}}) {
+    return console.log(`${endereco} ${numero}`);
+}
+enderecoCompleto(pessoa);
+
+
+
