77a7965cd3e79311057303d842f699ba631b58fa
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2024-01-21 02:29:24 -0300

    updates

--- ReactJs/galeria-de-fotos/src/app/page.tsx
+++ ReactJs/galeria-de-fotos/src/app/page.tsx
@@ -1,22 +1,46 @@
 "use client"
 
+import { Modal } from "@/components/Modal";
 import { PhotoItem } from "@/components/PhotoItem";
 import { photoList } from "@/data/photoList";
+import { useState } from "react";
 
 const Page = () => {
+  
+  const [showModal, setShowModal] = useState(false);
+  const [imageOfModal, setImageOfModal] = useState('');
+
+  const openModal = (id: number) => {
+    const photo = photoList.find(item => item.id === id);
+    if (photo) {
+      setImageOfModal(photo.url);
+      setShowModal(true);
+    }
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
+
   return (
-    <div className="w-screen h-max p-10 flex justify-center items-center bg-black">
-      <div className="container">
-          <h1 className="text-white font-bold text-center text-2xl mb-28">Fotos Intergalaticas</h1>
-          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 mx-auto">
+    <div className="w-screen h-max flex justify-center items-center bg-black">
+      <div className="container p-10">
+          <h1 className="text-white font-bold text-center text-2xl mb-5">Fotos Intergalaticas</h1>
+          <div className="grid grid-cols-1 max-w-5xl gap-8 md:grid-cols-2 lg:grid-cols-3 mx-auto">
               {photoList.map(item=>(
                   <PhotoItem 
                   key={item.id}
                   photo={item}
-                  onClick={() => { }}
+                  onClick={() => openModal(item.id)}
                   />
               ))};
           </div>
+          {showModal &&
+            <Modal 
+            image={imageOfModal}
+            closeModal={closeModal}
+            />
+          }
       </div>
     </div>
   );
---
+++ ReactJs/galeria-de-fotos/src/types/ModalProps.ts
@@ -0,0 +1,4 @@
+export type ModalProps = {
+    image: string;
+    closeModal: () => void;
+}
